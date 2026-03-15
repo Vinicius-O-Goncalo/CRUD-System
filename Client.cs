de8a860d4f6ec78e29a36d263afbf7b40044aef5
Vinícius O. Gonçalo <goncalovinicius15@gmail.com>
2026-03-15 19:40:00 -0300

    Add Client model with Id, Name, and Email properties

---
+++ Client.cs
@@ -0,0 +1,11 @@
+namespace CrudApp.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
