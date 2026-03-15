0d9418bc7e9148a2f684528f181498f888927e48
Vinícius O. Gonçalo <goncalovinicius15@gmail.com>
2026-03-15 19:41:12 -0300

    Implement CRUD operations for Clients in Database class

---
+++ Database.cs
@@ -0,0 +1,74 @@
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CrudApp.Data
+{
+    public class Database
+    {
+        private string connectionString =
+            "Server=localhost;Database=CrudDB;Trusted_Connection=True;";
+
+        public DataTable GetClients()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Clients";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataTable table = new DataTable();
+
+                adapter.Fill(table);
+
+                return table;
+            }
+        }
+
+        public void InsertClient(string name, string email)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Clients (Name, Email) VALUES (@Name,@Email)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@Name", name);
+                cmd.Parameters.AddWithValue("@Email", email);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdateClient(int id, string name, string email)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Clients SET Name=@Name, Email=@Email WHERE Id=@Id";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.Parameters.AddWithValue("@Name", name);
+                cmd.Parameters.AddWithValue("@Email", email);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteClient(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Clients WHERE Id=@Id";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
